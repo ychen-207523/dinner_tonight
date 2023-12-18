ac6ba46c220feba45006e2837e63ca775b81dfc6
ychen-207523 <ychen207523@gmail.com>
2023-12-18 16:43:05 -0500

    improve the style

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
     var mealList = document.getElementById('mealList');
     const selectButton = document.getElementById('selectButton');
+    const addButton = document.getElementById('addButton');
+    const mealInput = document.getElementById('mealInput');
     const resultText = document.getElementById('result');
+    let allMeals = [];
+
+    function createListItem(meal) {
+        const li = document.createElement('li');
+        li.textContent = meal;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        deleteButton.onclick = function () {
+            allMeals = allMeals.filter(m => m !== meal);
+            li.remove();
+        };
+
+        li.appendChild(deleteButton);
+        return li;
+    }
 
     fetch('meals.json')
         .then(response => response.json())
         .then(data => {
+            const ul = document.createElement('ul');
             if (data && data.meals && data.meals.length > 0) {
-
-                const ul = document.createElement('ul');
-                data.meals.forEach(element => {
-                    const li = document.createElement('li');
-                    li.textContent = element;
+                allMeals = data.meals;
+                allMeals.forEach(element => {
+                    const li = createListItem(element);
                     ul.appendChild(li);
                 });
-
-                mealList.appendChild(ul);
             }
-        })
+            mealList.appendChild(ul);
+        });
 
     selectButton.addEventListener('click', () => {
-        fetch('meals.json')
-            .then(response => response.json())
-            .then(data => {
-                if (data && data.meals && data.meals.length > 0) {
-                    const randomIndex = Math.floor(Math.random() * data.meals.length);
-                    const randomMeal = data.meals[randomIndex];
-                    resultText.textContent = `Tonight, you should have ${randomMeal} for dinner!`;
-                } else {
-                    resultText.textContent = 'No dinner options available.';
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+        if (allMeals.length > 0) {
+            const randomIndex = Math.floor(Math.random() * allMeals.length);
+            const selectedMeal = allMeals[randomIndex];
+            resultText.textContent = 'Selected Meal: ' + selectedMeal;
+        } else {
+            resultText.textContent = 'No meals available to select.';
+        }
+
     });
+
+    addButton.addEventListener('click', () => {
+        const newMeal = mealInput.value.trim();
+        if (newMeal) {
+            allMeals.push(newMeal);
+            const li = createListItem(newMeal);
+            mealList.querySelector('ul').appendChild(li);
+            mealInput.value = ''; // Clear the input field
+        }
+
+    })
 });
